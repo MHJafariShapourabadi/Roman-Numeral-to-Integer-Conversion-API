364093f63737c52904915cffec04bdf2800a3c30
Mohammadhossein Jafari Shapourabadi <68470568+MHJafariShapourabadi@users.noreply.github.com>
2024-10-16 01:59:04 +0330

    Modify invalid roman numeral exception handling

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class RomanNumber(BaseModel):
 # Conversion function
 def roman_to_int(roman: str) -> int:
     roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_prev_char = {'V': 'I', 'X': 'I', 'L': 'X', 'C': 'X', 'D': 'C', 'M': 'C'}
+    roman_prev_char = {'V': 'I', 'X': 'IX', 'L': 'X', 'C': 'XC', 'D': 'C', 'M': 'CM'}
     result = 0
     prev_value = 0
     prev_char = None
@@ -18,15 +18,16 @@ def roman_to_int(roman: str) -> int:
     try:
         for char in reversed(roman):
             current_value = roman_values[char]
+            if current_value <= prev_value and char not in roman_prev_char[prev_char]:
+                raise ValueError
             if current_value < prev_value:
-                if char != roman_prev_char[prev_char]:
-                    raise KeyError
                 result -= current_value
+                current_value = prev_value - current_value
             else:
                 result += current_value
             prev_value = current_value
             prev_char = char
-    except KeyError:
+    except (KeyError, ValueError):
         raise HTTPException(status_code=400, detail="Invalid Roman numeral")
     
     return result
