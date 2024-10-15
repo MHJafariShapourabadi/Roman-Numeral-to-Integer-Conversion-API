83f20d9660f443f2c4621b58c83903ca09cf04e0
Mohammadhossein Jafari Shapourabadi <68470568+MHJafariShapourabadi@users.noreply.github.com>
2024-10-16 03:08:05 +0330

    Change calculations

--- main.py
+++ main.py
@@ -10,27 +10,40 @@ class RomanNumber(BaseModel):
 # Conversion function
 def roman_to_int(roman: str) -> int:
     roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_prev_char = {'I': 'I', 'V': 'I', 'X': 'IX', 'L': 'X', 'C': 'XC', 'D': 'C', 'M': 'CM'}
+    roman_prev_char = {'V': 'I', 'X': 'I', 'L': 'X', 'C': 'X', 'D': 'C', 'M': 'C'}
     result = 0
-    prev_value = 0
+    prev_value = float("inf")
     prev_char = None
     
     try:
-        for char in reversed(roman):
+        if "VV" in roman or "LL" in roman or "DD" in roman:
+            raise ValueError
+        for i in range(len(roman)):
+            i_prev = i - 1 if i != 0 else None
+            i_next = i + 1 if i != len(roman) - 1 else None
+            char = roman[i]
             current_value = roman_values[char]
-            if current_value <= prev_value and char not in roman_prev_char[prev_char]:
-                raise ValueError
-            if current_value < prev_value:
-                result -= current_value
-                current_value = prev_value - current_value
+            if current_value > prev_value:
+                if i_prev is not None:
+                    prev_char = roman[i_prev]
+                    if prev_char != roman_prev_char[char]:
+                        raise ValueError
+                    result -= prev_value
+                    current_value -= prev_value
+                    result += current_value
+                    if i_next is not None:
+                        next_char = roman[i_next]
+                        if next_char == prev_char:
+                            raise ValueError
             else:
                 result += current_value
             prev_value = current_value
             prev_char = char
     except (KeyError, ValueError):
-        raise HTTPException(status_code=400, detail="Invalid Roman numeral")
+        raise Exception("Invalid Roman numeral")
     
     return result
+    
 # Define the API
 @app.post("/convert/")
 async def convert_roman_to_integer(input_data: RomanNumber):
